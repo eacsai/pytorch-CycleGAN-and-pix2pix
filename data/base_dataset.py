c8c63acf69324a05ab0e48ff2208a92d9047f4b9
eacsai <1349157283@qq.com>
2024-04-20 18:37:59 +0800

    feat: init program

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -83,8 +83,7 @@ def get_transform(opt, params=None, grayscale=False, method=transforms.Interpola
     if grayscale:
         transform_list.append(transforms.Grayscale(1))
     if 'resize' in opt.preprocess:
-        osize = [opt.load_size, opt.load_size]
-        transform_list.append(transforms.Resize(osize, method))
+        transform_list.append(transforms.Resize(opt.load_size, method))
     elif 'scale_width' in opt.preprocess:
         transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.load_size, opt.crop_size, method)))
 
